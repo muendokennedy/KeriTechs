35019193c4670358a94e26e753aefc00a4139799
muendokennedy <kennedymunyao999@gmai.com>
2024-11-04 15:29:19 +0300

    Implemented the logic of adding the error display logic in product upload admin page

--- resources/js/Pages/Admin/AdminProducts.vue
+++ resources/js/Pages/Admin/AdminProducts.vue
@@ -7,6 +7,13 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import {isImage} from '@/helpers.js'
 import { useForm} from '@inertiajs/vue3'
 
+const props = defineProps({
+  product: {
+    type: Array,
+    required: true
+  }
+})
+
 
 import {
   TransitionRoot,
@@ -17,6 +24,8 @@ import {
 } from '@headlessui/vue'
 
 const isOpen = ref(false)
+const formErrors = ref([])
+const flashMessage = ref('')
 
 const form = useForm({
     productCategory: '',
@@ -53,8 +62,10 @@ form.attachments = attachmentFiles.value.map(myFile => myFile.file)
 
 form.post(route('admin.product.store'), {
   onError: (errors) => {
-    console.log(errors)
-  }
+    formErrors.value.push(errors)
+    console.log(formErrors.value[0].productName)
+  },
+  preserveScroll: true
 })
 
 }
@@ -70,21 +81,36 @@ async function onImageChoose(event){
         attachmentFiles.value.push(myFile)
         if(attachmentFiles.value.length > 4){
             attachmentErrors.value.push('You must select exactly 4 images')
+              setTimeout(() => {
+              flashMessage.value = ''
+              }, 4000)
+              flashMessage.value = 'You must select exactly 4 images'
             attachmentFiles.value = []
+            event.target.files = null
             return
         } else{
             attachmentErrors.value = []
+            flashMessage.value = ''
+            event.target.files = null
         }
     }
     if(attachmentFiles.value.length < 4){
         attachmentErrors.value.push('You must select exactly 4 images')
+        setTimeout(() => {
+        flashMessage.value = ''
+        }, 4000)
+        flashMessage.value = 'You must select exactly 4 images'
         attachmentFiles.value = []
+        event.target.files = null
         return
     } else {
         attachmentErrors.value = []
+        flashMessage.value = ''
+        event.target.files = null
     }
-    event.target.files
-}
+    event.target.files = null
+  }
+
 
  async function readFile(file){
     return new Promise((res,rej) => {
@@ -262,32 +288,53 @@ async function onImageChoose(event){
         <div class="new-product bg-white p-4 rounded-md my-4">
           <h2 class="text-[rgb(4,46,255)] font-semibold text-base md:text-xl py-4 capitalize">add new product</h2>
           <div class="new-product-form">
+            <div class="flash-message transition-all duration-300 flex justify-between text-white bg-red-500 border-red-700 border rounded-md fixed top-20 right-12 w-[600px] p-4 z-10"
+            :class="[
+              !flashMessage ? '-right-[100%]' : 'right-20'
+            ]">{{flashMessage}}
+              <XMarkIcon class="size-6 cursor-pointer"/>
+            </div>
             <form @submit.prevent="submit">
               <div class="form-row w-full flex flex-col md:flex-row justify-between">
                 <div class="input-box md:basis-[48%]">
                   <label for="category" class="block py-3">Select Category:</label>
-                  <select v-model="form.productCategory" id="category" class="border-2 outline-none rounded-md px-4 py-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out" autofocus>
+                  <select v-model="form.productCategory" id="category" class="border-2 outline-none rounded-md px-4 py-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out" autofocus
+                  :class="[
+                    formErrors[0]?.productCategory ? 'border-red-500' : ''
+                  ]">
                     <option value="None">None</option>
                     <option value="Phone">Phone</option>
                     <option value="Laptop">Laptop</option>
                     <option value="Smartwatch">Smartwatch</option>
                     <option value="Television">Television</option>
                   </select>
+                  <p v-if="formErrors[0]?.productCategory" class="text-red-500">{{ formErrors[0]?.productCategory }}</p>
                 </div>
                 <div class="input-box md:basis-[48%]">
                   <label for="productName" class="block py-3">Enter Product name:</label>
-                  <input type="text" v-model="form.productName" id="productName" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out" autofocus>
-                  <p class="text-red-500 text-sm">The product name is required</p>
+                  <input type="text" v-model="form.productName" id="productName" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out" autofocus
+                  :class="[
+                    formErrors[0]?.productName ? 'border-red-500' : ''
+                  ]">
+                  <p v-if="formErrors[0]?.productName" class="text-red-500">{{ formErrors[0]?.productName }}</p>
                 </div>
               </div>
               <div class="form-row w-full flex flex-col md:flex-row justify-between">
                 <div class="input-box md:basis-[48%]">
                   <label for="specs" class="block py-3">Enter product specifications (CSV):</label>
-                  <input type="text" v-model="form.productSpecifications" id="specs" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out" autofocus>
+                  <input type="text" v-model="form.productSpecifications" id="specs" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out" autofocus
+                  :class="[
+                    formErrors[0]?.productSpecifications ? 'border-red-500' : ''
+                  ]">
+                  <p v-if="formErrors[0]?.productSpecifications" class="text-red-500">{{ formErrors[0]?.productSpecifications }}</p>
                 </div>
                 <div class="input-box md:basis-[48%]">
                   <label for="initialPrice" class="block py-3">Enter the initial price:</label>
-                  <input type="number" v-model="form.productInitialPrice" id="initialPrice" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out" autofocus>
+                  <input type="number" v-model="form.productInitialPrice" id="initialPrice" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out" autofocus
+                  :class="[
+                    formErrors[0]?.productInitialPrice ? 'border-red-500' : ''
+                  ]">
+                  <p v-if="formErrors[0]?.productInitialPrice" class="text-red-500">{{ formErrors[0]?.productInitialPrice }}</p>
                 </div>
               </div>
               <div class="my-4">
@@ -318,33 +365,53 @@ async function onImageChoose(event){
                         </div>
                     </div>
                 </div>
-                <div v-if="attachmentErrors" class="mt-1 text-sm text-red-500">{{ attachmentErrors[0] }}</div>
+                <!-- <div v-if="attachmentErrors" class="mt-1 text-sm text-red-500">{{ attachmentErrors[0] }}</div> -->
+                <p v-if="formErrors[0]?.attachments" class="text-red-500">{{ formErrors[0]?.attachments }}</p>
               </div>
               <div class="form-row w-full flex flex-col md:flex-row justify-between">
                 <div class="input-box md:basis-[48%]">
                   <label for="discountPrice" class="block py-3">Enter the discounted price:</label>
-                  <input type="number" v-model="form.productDiscountPrice" id="discountPrice" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out" autofocus>
+                  <input type="number" v-model="form.productDiscountPrice" id="discountPrice" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out" autofocus
+                  :class="[
+                    formErrors[0]?.productDiscountPrice ? 'border-red-500' : ''
+                  ]">
+                  <p v-if="formErrors[0]?.productDiscountPrice" class="text-red-500">{{ formErrors[0]?.productDiscountPrice }}</p>
                 </div>
                 <div class="input-box md:basis-[48%]">
                   <label for="brandName" class="block py-3">Enter the brand name:</label>
-                  <input type="text" v-model="form.brandName" id="brandName" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out" autofocus>
+                  <input type="text" v-model="form.brandName" id="brandName" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out" autofocus
+                  :class="[
+                    formErrors[0]?.brandName ? 'border-red-500' : ''
+                  ]">
+                  <p v-if="formErrors[0]?.brandName" class="text-red-500">{{ formErrors[0]?.brandName }}</p>
                 </div>
               </div>
               <div class="form-row w-full flex flex-col md:flex-row justify-between">
                 <div class="input-box md:basis-[48%]">
                   <label for="discountPrice" class="block py-3">Enter product average rating:</label>
-                  <input type="number" v-model="form.productAverageRating" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out" autofocus>
+                  <input type="number" v-model="form.productAverageRating" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out" autofocus
+                  :class="[
+                    formErrors[0]?.productAverageRating ? 'border-red-500' : ''
+                  ]">
+                  <p v-if="formErrors[0]?.productAverageRating" class="text-red-500">{{ formErrors[0]?.productAverageRating }}</p>
                 </div>
                 <div class="input-box md:basis-[48%]">
                   <label for="brandName" class="block py-3">Enter the product warranty:</label>
-                  <input type="text" v-model="form.productWarranty" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out" autofocus>
+                  <input type="text" v-model="form.productWarranty" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out" autofocus
+                  :class="[
+                    formErrors[0]?.productWarranty ? 'border-red-500' : ''
+                  ]">
+                  <p v-if="formErrors[0]?.productWarranty" class="text-red-500">{{ formErrors[0]?.productWarranty }}</p>
                 </div>
               </div>
               <div class="form-row">
                 <div class="input-box">
                   <label for="product-description" class="block py-3">Type the product description(End the first sentence with pipe(|) then period(.)):</label>
-                  <ckeditor :editor="editor" v-model="form.productDescription" :config="editorConfig"></ckeditor>
+                  <ckeditor :editor="editor" v-model="form.productDescription" :config="editorConfig"   :class="[
+                    formErrors[0]?.productWarranty ? 'border-red-500' : ''
+                  ]" ></ckeditor>
                   <!--<textarea name="productDescription" id="product-description" cols="30" rows="10" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out" autofocus></textarea>---->
+                  <p v-if="formErrors[0]?.productDescription" class="text-red-500">{{ formErrors[0]?.productDescription }}</p>
                 </div>
               </div>
               <button type="submit" class="capitalize px-4 py-2 bg-[#042EFF] rounded-md text-white my-4">add product</button>
